7a05a7ef838e146bc6e7886e62863ae2a131d44f
robertshum <robertshum@gmail.com>
2024-04-28 02:45:36 -0700

    use forms for add/edit powers

--- src/components/AddEditPowerForm.tsx
+++ src/components/AddEditPowerForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, MouseEvent } from "react";
+import { ChangeEvent, FormEvent, MouseEvent } from "react";
 
 type AddEditPowerFormProps = {
   titleName: string;
@@ -8,7 +8,7 @@ type AddEditPowerFormProps = {
   isButtonDisabled: boolean;
   handleOnChangeName: (e: ChangeEvent<HTMLInputElement>) => void;
   handleOnChangeDescription: (e: ChangeEvent<HTMLTextAreaElement>) => void;
-  handleOnSubmit: (e: MouseEvent<HTMLButtonElement>) => void;
+  handleOnSubmit: (e: MouseEvent<HTMLButtonElement> | FormEvent<HTMLFormElement>) => void;
 };
 
 const AddEditPowerForm = (props: AddEditPowerFormProps) => {
@@ -18,32 +18,38 @@ const AddEditPowerForm = (props: AddEditPowerFormProps) => {
       <div className="flex-1 overflow-y-auto sm:pt-2 md:pt-2 pt-4 pb-4 px-6">
         <article className="mt-10 mb-2 gap-6">
           <h1 className="stat-value">{props.titleName}</h1>
-          <div className="flex flex-col min-w-3 items-start gap-5 flex-wrap mt-5">
-            {/* Tag */}
-            <label className="input input-bordered min-w-96 flex items-center">
-              Power Name:
-              <input type="text"
-                value={props.powerName}
-                onChange={props.handleOnChangeName} className="grow ml-2" placeholder="Laser Eyes" />
-            </label>
-            {/* Description */}
-            <label className="form-control ">
-              <div className="label">
-                <span className="label-text">Description:</span>
-              </div>
-              <textarea value={props.description}
-                onChange={props.handleOnChangeDescription}
-                className="textarea textarea-bordered h-24 min-w-96" placeholder="Zap enemies with laser eyes."></textarea>
-            </label>
-            {/* Create */}
-            <button
-              disabled={props.isButtonDisabled}
-              onClick={props.handleOnSubmit}
-              className="btn btn-md btn-ghost text-md">{props.buttonName}</button>
-          </div>
+          
+          <form onSubmit={props.handleOnSubmit}>
+            <div className="flex flex-col min-w-3 items-start gap-5 flex-wrap mt-5">
+              {/* Tag */}
+              <label className="input input-bordered min-w-96 flex items-center">
+                Power Name:
+                <input type="text"
+                  value={props.powerName}
+                  onChange={props.handleOnChangeName} className="grow ml-2" placeholder="Laser Eyes" />
+              </label>
+              {/* Description */}
+              <label className="form-control ">
+                <div className="label">
+                  <span className="label-text">Description:</span>
+                </div>
+                <textarea value={props.description}
+                  onChange={props.handleOnChangeDescription}
+                  className="textarea textarea-bordered h-24 min-w-96" placeholder="Zap enemies with laser eyes."></textarea>
+              </label>
+              {/* Create */}
+              <button
+                type="submit"
+                disabled={props.isButtonDisabled}
+                onClick={props.handleOnSubmit}
+                className="btn btn-md btn-ghost text-md">{props.buttonName}</button>
+              {/* <input type="submit" value="Submit" /> */}
+            </div>
+          </form>
+
         </article>
       </div>
-    </div>
+    </div >
   );
 };
 
