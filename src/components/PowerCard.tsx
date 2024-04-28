edec05d061a98351d1a507f3d35c5c4f8c681b26
robertshum <robertshum@gmail.com>
2024-04-27 23:42:09 -0700

    add and edit power feature

--- src/components/PowerCard.tsx
+++ src/components/PowerCard.tsx
@@ -13,7 +13,7 @@ function PowerCard(props: {
     <div className="card bg-base-100 shadow-xl p-1 min-h-28">
       <div className="card-body">
         <div className="badge badge-info">{props.tag}</div>
-        <p className="line-clamp-2">{props.description}</p>
+        <p className="line-clamp-1">{props.description}</p>
         <div className="card-actions justify-end">
           <button onClick={props.viewClickHandler}
             className="btn btn-ghost btn-sm">View</button>
--- src/pages/AddPower.tsx
+++ src/pages/AddPower.tsx
@@ -1,9 +1,54 @@
+import { useState, ChangeEvent, MouseEvent } from "react";
+import { usePostPowerAPI } from "../hooks/useAPI";
+import AddEditPowerForm from "../components/AddEditPowerForm";
+
 const AddPower = () => {
 
+  const [powerName, setPowerName] = useState("");
+  const [description, setDescription] = useState("");
+  const { addPower, addPowerIsLoading, addPowerError } = usePostPowerAPI();
+
+  // track name changes
+  const handleOnChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.value;
+    setPowerName(name);
+  };
+
+  // track desc changes
+  const handleOnChangeDescription = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const desc = e.target.value;
+    setDescription(desc);
+  };
+
+  // submit, post req, redirect
+  const handleOnSubmit = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    //start adding power
+    addPower({
+      tag: powerName,
+      description
+    });
+  };
+
+  // disabled button
+  const isDisabled = !powerName || !description;
+
+  // TODO make loading/error better looking :)
+  if (addPowerIsLoading) return <div>creating power...</div>;
+  if (addPowerError) return <div>error creating power...</div>;
+
   return (
-    <>
-      <h1>Add Power</h1>
-    </>
+    <AddEditPowerForm
+      titleName="Add Power"
+      buttonName="Add Power"
+      powerName={powerName}
+      description={description}
+      isButtonDisabled={isDisabled}
+      handleOnChangeName={handleOnChangeName}
+      handleOnChangeDescription={handleOnChangeDescription}
+      handleOnSubmit={handleOnSubmit}
+    />
   );
 };
 
