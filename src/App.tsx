786dbce49d3d6d7a969800d0b3c03fb162c4f0fa
robertshum <robertshum@gmail.com>
2024-05-12 15:15:27 -0700

    init clerk in framework

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,33 @@ import AddPower from './pages/AddPower';
 import EditPower from './pages/EditPower';
 import AddSuperhero from './pages/AddSuperhero';
 import EditSuperhero from './pages/EditSuperhero';
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+
 
 function App() {
 
   return (
     <>
-      <Navigation />
-      <Routes>
-        {/* any home '/' or 'SuperHeroDashboard' redirects to dashboard */}
-        <Route path="/SuperheroDashboard"
-          element={<Navigate to="/" replace />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/powers" element={<Powers />} />
-        <Route path="/superheroes" element={<Superheroes />} />
-        <Route path="/power/:powerId" element={<Power />} />
-        <Route path="/power/edit/:powerId" element={<EditPower />} />
-        <Route path="/power/add/" element={<AddPower />} />
-        <Route path="/hero/:superHeroId" element={<Superhero />} />
-        <Route path="/hero/edit/:superHeroId" element={<EditSuperhero />} />
-        <Route path="/hero/add" element={<AddSuperhero />} />
-      </Routes>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <Navigation />
+        <Routes>
+          {/* any home '/' or 'SuperHeroDashboard' redirects to dashboard */}
+          <Route path="/SuperheroDashboard"
+            element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/powers" element={<Powers />} />
+          <Route path="/superheroes" element={<Superheroes />} />
+          <Route path="/power/:powerId" element={<Power />} />
+          <Route path="/power/edit/:powerId" element={<EditPower />} />
+          <Route path="/power/add/" element={<AddPower />} />
+          <Route path="/hero/:superHeroId" element={<Superhero />} />
+          <Route path="/hero/edit/:superHeroId" element={<EditSuperhero />} />
+          <Route path="/hero/add" element={<AddSuperhero />} />
+        </Routes>
+      </SignedIn>
       <Footer />
     </>
   );
